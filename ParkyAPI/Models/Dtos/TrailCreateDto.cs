166be8eb647d0c2ddb20d61d5bf0f1ab106fa865
Rajat Pandey <rajat121237@gmail.com>
2022-06-05 23:08:40 +0530

    Added MVC Web Based Project.

---
+++ ParkyAPI/Models/Dtos/TrailCreateDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkyAPI.Models.Dtos
+{
+    public class TrailCreateDto
+    {
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Distance { get; set; }
+
+        public DifficultyType Difficulty;
+
+        [Required]
+        public int NationalParkId { get; set; }
+    }
+}
---
+++ ParkyAPI/Models/Dtos/TrailDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkyAPI.Models.Dtos
+{
+    public class TrailDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Distance { get; set; }
+
+        public DifficultyType Difficulty;
+
+        [Required]
+        public int NationalParkId { get; set; }
+
+        public NationalParkDto NationalPark { get; set; }
+    }
+}
